16bce89cd906a0e09435ef7d296fe0535942b344
Admin <gholmzadehs@gmail.com>
2022-04-06 09:57:31 +0430

    Repozitory Initialized

---
+++ Dayana/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dayana.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
